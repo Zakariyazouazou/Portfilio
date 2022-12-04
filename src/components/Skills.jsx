b7681a62fb42c13028a41162b0d76865921378d7
Zakariyazouazou <112694786+Zakariyazouazou@users.noreply.github.com>
2022-12-04 19:10:49 +0100

    first commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,7 +17,7 @@ import statistiques from '../assets/statistiques.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-orange-300 text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-orange-300 text-gray-300 mt-14'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
